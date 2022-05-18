0d161616d80435c5e54ca53b136c87c454ec781a
MatMasIt <mattia.mascarello@outlook.it>
2022-05-18 08:07:27 +0200

    Fixed minor error

--- gitExport.sh
+++ gitExport.sh
@@ -11,7 +11,7 @@ s=$(cat $lastDir"/smoke.csv" | tail -n 1 | awk -F","  '{ print $2 } ')
 pm10=$(cat $lastDir"/pm10.csv" | tail -n 1 | awk -F","  '{ print $2 } ')
 pm25=$(cat $lastDir"/pm25.csv" | tail -n 1 | awk -F","  '{ print $2 } ')
 utciso=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-echo -e  '{\n"T":'$t',\n"H":'$h',\n"P":'$p',\n"PM10":'$pm10',\n"PM25":'$pm25',\n"S":'$s',\n"utciso":"'$utciso'"\n}' | tee last.json
+printf  '{\n"T":'$t',\n"H":'$h',\n"P":'$p',\n"PM10":'$pm10',\n"PM25":'$pm25',\n"S":'$s',\n"utciso":"'$utciso'"\n}' | tee last.json
 git pull
 git add *
 git commit -m "Regular data update"
