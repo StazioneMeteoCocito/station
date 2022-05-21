c4f05139c510fa0f8489f7cb5f1fe7a20a2297ec
MatMasIt <mattia.mascarello@outlook.it>
2022-05-21 16:28:07 +0200

    Updated single use and added async

--- bullet/bulletin.sh
+++ bullet/bulletin.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-while :
-do
-	python3 radioupdate.py
-	espeak -s 120 -f bullEN -w en.wav
-	espeak -v it -s 120 -f bullIT -w it.wav
-	cat radio | minimodem --tx rtty -f radio.wav
-	ffmpeg -y -i en.wav -i it.wav -i radio.wav \
-	-filter_complex '[0:0][1:0][2:0]concat=n=3:v=0:a=1[out]' \
-	-map '[out]' bulletin.mp3
-	rm *.wav
-	mv bulletin.mp3 ../storedData/
-	espeak -s 120 -f bullEN
-	espeak -v it -s 120 -f bullIT
-	cat radio | minimodem --tx rtty
-	sleep 10m
-done
+python3 radioupdate.py
+espeak -s 120 -f bullEN -w en.wav
+espeak -v it -s 120 -f bullIT -w it.wav
+cat radio | minimodem --tx rtty -f radio.wav
+ffmpeg -y -i en.wav -i it.wav -i radio.wav \
+-filter_complex '[0:0][1:0][2:0]concat=n=3:v=0:a=1[out]' \
+-map '[out]' bulletin.mp3
+rm *.wav
+mv bulletin.mp3 ../storedData/
+(espeak -s 120 -f bullEN &&
+espeak -v it -s 120 -f bullIT &&
+cat radio | minimodem --tx rtty) &
