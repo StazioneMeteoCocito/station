e6e52eac9dbf93aa7f851cee591b0cd5eaf9b470
Mattia Mascarello <mattia.mascarello@outlook.it>
2022-07-10 23:14:54 +0200

    logging

--- tmux.sh
+++ tmux.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo " [stationWrapper] Starting station main software" | xargs -d"\n" -I {} date +"%Y-%m-%d %H:%M:%S {}" >> /home/pi/station.log
 tmux new -s StazioneMeteo -d /home/pi/Desktop/station/boot.sh
 # Saved in crontab (root)
