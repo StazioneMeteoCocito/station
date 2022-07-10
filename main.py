2bb11d59c0442caf7a66d56022033a840c78e741
Mattia Mascarello <mattia.mascarello@outlook.it>
2022-07-10 23:13:52 +0200

    Update main

--- main.py
+++ main.py
@@ -8,23 +8,33 @@ import datetime
 import signal
 sense = SenseHat()
 s = Station()
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
-running = True
+running = False
 
 def log(text):
     f = open("/home/pi/station.log", "a+")
-    f.write(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")+" [pythonStation] "+text)
+    f.write(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")+"  [pythonStation] "+text+"\n")
     f.close()
     
 
-def stop():
+def stop(a,b):
     global running
     log("Gracefully handling death")
-    running = True
+    running = False
 
 signal.signal(signal.SIGINT, stop)
 signal.signal(signal.SIGTERM, stop)
 
+ttyflag = False
+for i in range(10):
+    try:
+        log("Trying /dev/ttyACM"+str(i))
+        ser = serial.Serial('/dev/ttyACM'+str(i), 9600, timeout=10)
+        ttyflag = True
+        break
+    except:
+        time.sleep(10)
+if(not ttyflag):
+    log("Arduino is dead, continuing with it disabled")
 
 def get_sensors_serial(ser):
     print("Requesting data from arduino")
@@ -55,19 +65,18 @@ while running:
         s.record_pressure(allData["pressure"])
         print("Pressure {p:.2f} hPa".format(p=allData["pressure"]))
         s.record_temperature(allData["temperature"])
-        arduino = get_sensors_serial(ser)
-        if arduino:
-            allData["serialOk"] = True
-            s.record_pm10(arduino["pm10"])
-            s.record_pm25(arduino["pm2.5"])
-            s.record_smoke(arduino["smoke"])
-            allData["pm10"] = arduino["pm10"]
-            allData["pm2.5"] = arduino["pm2.5"]
-            allData["smoke"] = arduino["smoke"]
-            print("Arduino data - PM10: {pmt:.2f}, PM 2.5: {pmtw:.2f}, Smoke: {s:.2f}".format(
-                pmt=arduino["pm10"], pmtw=arduino["pm2.5"], s=arduino["smoke"]))
-        else:
-            print("Arduino not present")
+        if ttyflag:
+            arduino = get_sensors_serial(ser)
+            if arduino:
+                allData["serialOk"] = True
+                s.record_pm10(arduino["pm10"])
+                s.record_pm25(arduino["pm2.5"])
+                s.record_smoke(arduino["smoke"])
+                allData["pm10"] = arduino["pm10"]
+                allData["pm2.5"] = arduino["pm2.5"]
+                allData["smoke"] = arduino["smoke"]
+                print("Arduino data - PM10: {pmt:.2f}, PM 2.5: {pmtw:.2f}, Smoke: {s:.2f}".format(
+                    pmt=arduino["pm10"], pmtw=arduino["pm2.5"], s=arduino["smoke"]))
         log("Measurement taken")
         i = 0
         while i<timeMeasure and running:
@@ -75,6 +84,7 @@ while running:
             i += 1
     except Exception as e:
         print("EXCEPTION:")
+        log("Error : "+str(e))
         print(e)
         i = 0
         while i<timeMeasure and running:
